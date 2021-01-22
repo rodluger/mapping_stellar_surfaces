e246ce468154e0e00400cf5bdfba7ab72ae9d954
Rodrigo Luger <rodluger@gmail.com>
2021-01-22 17:29:32 -0500

    more text

---
+++ paper1/tests/test_svd.py
@@ -0,0 +1,64 @@
+import numpy as np
+from scipy.linalg import svd
+
+
+def test_svd():
+    # Number of terms in output vector
+    K = 100
+
+    # Number of terms in input vector
+    N = 50
+
+    # Rank of the operator
+    R = 25
+
+    # Random design matrix of shape (K, N) and rank R
+    np.random.seed(0)
+    A = np.random.randn(K, R) @ np.random.randn(R, N)
+
+    # SVD decomposition of A
+    U, S, VT = svd(A)
+
+    # Preimage operator
+    VTx = VT[:R]
+    PRE = VTx.T @ VTx
+
+    # Null space operator
+    VTo = VT[R:]
+    NULL = VTo.T @ VTo
+
+    # Random input vector
+    y = np.random.randn(N)
+
+    # Preimage component of input vector
+    yx = PRE @ y
+
+    # Null space component of input vector
+    yo = NULL @ y
+
+    # Output vector
+    f = A @ y
+
+    # Least squares solution
+    yls = np.linalg.lstsq(A, f, rcond=1e-12)[0]
+
+    # Check that the least squares solution IS the preimage
+    assert np.allclose(yx, yls)
+
+    # Check the orthogonality of VTx
+    assert np.allclose(VTx @ VTx.T, np.eye(R))
+
+    # Check the orthogonality of VTo
+    assert np.allclose(VTo @ VTo.T, np.eye(N - R))
+
+    # Check that P + N = I
+    assert np.allclose(PRE + NULL, np.eye(N, N))
+
+    # Check that yx + yo = y
+    assert np.allclose(yx + yo, y)
+
+    # Check that the preimage maps onto the flux
+    assert np.allclose(f, A @ yx)
+
+    # Check that the nullspace maps onto the zero vector
+    assert np.allclose(0, A @ yo)
